c265e7fe937338e83afcb6fe0e14f238f461f77a
michalbr0 <132956778+michalbr0@users.noreply.github.com>
2024-05-30 14:24:44 +0200

    Implemented std::memcpy

--- src/kernel/xstd/cstring.cpp
+++ src/kernel/xstd/cstring.cpp
@@ -10,4 +10,13 @@ namespace LiOS86::xstd {
         return *reinterpret_cast<const unsigned char*>(lhs) - *reinterpret_cast<const unsigned char*>(rhs);
     }
 
+    auto memcpy(void* dest, const void* src, std::size_t count) -> void* {
+        auto destBytePtr = reinterpret_cast<unsigned char*>(dest);
+        auto srcBytePtr = reinterpret_cast<const unsigned char*>(src);
+        while(count--) {
+            *(destBytePtr++) = *(srcBytePtr++);
+        }
+        return dest;
+    }
+
 }
--- src/kernel/xstd/cstring.hpp
+++ src/kernel/xstd/cstring.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <cstddef>
+
 namespace LiOS86::xstd {
 
     auto strcmp(const char* lhs, const char* rhs) -> int;
+    auto memcpy(void* dest, const void* src, std::size_t count) -> void*;
 
 }
